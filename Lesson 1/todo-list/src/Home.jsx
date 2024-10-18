3c9792a78694780d981497864779f1381b8935fe
EdwardThapelo <edwardthapelo55@gmail.com>
2024-10-18 14:44:02 +0200

    Added styles for the list of todos

--- Lesson 1/todo-list/src/Home.jsx
+++ Lesson 1/todo-list/src/Home.jsx
@@ -1,15 +1,22 @@
-import React, { useState, useEffect} from "react";
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {BsCircleFill} from 'react-icons/bs'
+import {BsFillTrashFill} from 'react-icons/bs'
 import Create from "./Create";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
 
-  useEffect(() =>{
-    axios.get('http://localhost:3001/get')
-    .then(result => setTodos(result.data))
-    .catch(err => console.log(err))
-  }, [])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get")
+      .then((result) => setTodos(result.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleEdit = () =>{
+
+  }
 
   return (
     <div className="home">
@@ -22,7 +29,20 @@ const Home = () => {
           <h2>No record</h2>
         </div>
       ) : (
-        todos.map((todo) => <div className="task">{todo.task}</div>)
+        todos.map((todo) => 
+        <div className="task">
+          <div className="checkbox" onClick={handleEdit}>
+            <BsCircleFill className='icon'/>
+               <p>{todo.task}</p>
+          </div>
+          <div>
+            <span><BsFillTrashFill className='icon' /></span>
+          </div>
+
+        </div>
+        
+
+          )
       )}
     </div>
   );
